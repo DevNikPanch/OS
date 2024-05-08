b285011778f7b2a0a222c830de5d7c7b5630f268
JuniorFront1 <sibiryakov_7@mail.ru>
2024-05-08 15:27:34 +0400

    fix insert TLB

--- OS(3)/main.c
+++ OS(3)/main.c
@@ -3,8 +3,10 @@
 
 
 #define PAGE_CAPACITY 1 << 8
-#define FRAME_SIZE 1 << 8
+#define FRAME_CAPACITY 1 << 8
 #define TLB_CAPACITY 16
+
+FILE *backing_store;
 
 struct page {
     int offset;
@@ -12,30 +14,28 @@ struct page {
     int frame_n;
 };
 
-FILE *backing_store;
-
 struct page tlb [TLB_CAPACITY]; // массив структур page для TLB
 struct page pages [PAGE_CAPACITY]; // массив структур page для хранения загруженных страниц
-char ram[PAGE_CAPACITY][FRAME_SIZE]; // массив для хранения фреймов
+char physical_ram[PAGE_CAPACITY][FRAME_CAPACITY]; // массив для хранения фреймов
 
 char tlb_len = 0;
 int frames_len = 0;
 int pages_len = 0;
-int processed_pages = 0; // счётчик обработанных страниц
+int processed_pages = 0;
 
 const int offset_mask = (1 << 8) - 1;
 
-int page_fault_cnt = 0; // счётчик ошибок
-int tlb_hit = 0; // счётчик попаданий в TLB
+int page_fault_cnt = 0;
+int tlb_hit = 0;
 
 // Загрузка данных страницы из файла в память, обновление информации о странице.
-int read_from_file(struct page *curr_page) {
+int read_data(struct page *curr_page) {
     if (frames_len >= PAGE_CAPACITY || pages_len >= PAGE_CAPACITY) {
         return -1;
     }
 
-    fseek(backing_store, curr_page->n * FRAME_SIZE, SEEK_SET);
-    fread(ram[frames_len], sizeof(char), FRAME_SIZE, backing_store);
+    fseek(backing_store, curr_page->n * FRAME_CAPACITY, SEEK_SET);
+    fread(physical_ram[frames_len], sizeof(char), FRAME_CAPACITY, backing_store);
 
     pages[pages_len].n = curr_page->n;
     pages[pages_len].frame_n = frames_len;
@@ -44,7 +44,7 @@ int read_from_file(struct page *curr_page) {
     return frames_len++;
 }
 
-// получает структуру page из логического адреса.
+// Получение структуры page из логического адреса.
 struct page get_page(int log_address) {
     struct page curr_page;
     curr_page.offset = log_address & offset_mask;
@@ -52,16 +52,18 @@ struct page get_page(int log_address) {
     return curr_page;
 }
 
-// вставляет страницу в TLB, обрабатывает замещение при переполнении.
+// Вставка страницы в TLB, обрабатывание замещения при переполнении.
 void insert_tlb(struct page *curr_page) {
     if (tlb_len == 16) {
+
         memmove(&tlb[0], &tlb[1], sizeof(struct page) * 15);
         tlb_len--;
+
     }
     tlb[tlb_len++] = *curr_page;
 }
 
-// получает фрейм для заданной страницы, обрабатывает TLB и страницы.
+// Получение фрейма для заданной страницы, обрабатывание TLB и страницы.
 int get_frame(struct page *curr_page) {
     char hit = 0;
 
@@ -83,7 +85,7 @@ int get_frame(struct page *curr_page) {
     }
 
     if (!hit) {
-        curr_page->frame_n = read_from_file(curr_page);
+        curr_page->frame_n = read_data(curr_page);
         ++page_fault_cnt;
     }
 
@@ -97,6 +99,8 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    int log_address = 0;
+
     backing_store = fopen("BACKING_STORE.bin", "rb");
     if (backing_store == NULL) {
         printf("BACKING_STORE.bin error\n");
@@ -105,17 +109,16 @@ int main(int argc, char *argv[]) {
 
     freopen("check.txt", "w", stdout);
 
-    int log_address = 0;
 
     while (scanf("%d", &log_address) == 1) {
         struct page curr_page = get_page(log_address);
         get_frame(&curr_page);
-        printf("Virtual address: %d Physical address: %d Value: %d\n", (curr_page.n << 8) | curr_page.offset, (curr_page.frame_n << 8) | curr_page.offset, ram[curr_page.frame_n][curr_page.offset]);
+        printf("Virtual address: %d Physical address: %d Value: %d\n", (curr_page.n << 8) | curr_page.offset, (curr_page.frame_n << 8) | curr_page.offset, physical_ram[curr_page.frame_n][curr_page.offset]);
         ++processed_pages;
     }
 
-    printf("\nPage fault frequency = %0.2f%%\n", page_fault_cnt * 100. / processed_pages);
-    printf("TLB frequency = %0.2f%%\n", tlb_hit * 100. / processed_pages);
+    printf("\nPage fault frequency = %0.3f%%\n", page_fault_cnt * 100. / processed_pages);
+    printf("TLB frequency = %0.3f%%\n", tlb_hit * 100. / processed_pages);
 
     return 0;
 }
